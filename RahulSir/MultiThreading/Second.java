4716301712d8467dcadd1467442b09c9fe2e34a1
nisarg0606 <nisarg0606@gmail.com>
2021-05-15 18:05:39 +0530

    commit

--- RahulSir/MultiThreading/Second.java
+++ RahulSir/MultiThreading/Second.java
@@ -1,23 +1,24 @@
 //program to perform multiple task by multiple thread
 package RahulSir.MultiThreading;
-class Resource1 extends Thread{
+
+class Resource1 extends Thread {
     @Override
     public void run() {
-        for (int i = 1; i <=10; i++) 
-		{
-			System.out.println(Thread.currentThread().getName() + " -- " +  1 + " * " + i + " = " + (1*i));
-		}
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(Thread.currentThread().getName() + " -- " + 1 + " * " + i + " = " + (1 * i));
+        }
     }
 }
-class Resource2 extends Thread{
+
+class Resource2 extends Thread {
     @Override
     public void run() {
-        for (int i = 1; i <=10; i++) 
-		{
-			System.out.println(Thread.currentThread().getName() + " -- " +  2 + " * " + i + " = " + (2*i));
-		}
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(Thread.currentThread().getName() + " -- " + 2 + " * " + i + " = " + (2 * i));
+        }
     }
 }
+
 public class Second {
     public static void main(String[] args) {
         Resource1 r1 = new Resource1();
