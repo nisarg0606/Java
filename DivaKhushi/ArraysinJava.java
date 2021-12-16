1d085cb8e54da989c82c1a4642de562345214d32
nisarg001 <nisarg0606@gmail.com>
2021-12-16 15:28:20 +0530

    arrays

---
+++ DivaKhushi/ArraysinJava.java
@@ -0,0 +1,21 @@
+package DivaKhushi;
+
+import java.util.Scanner;
+
+public class ArraysinJava {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of n");
+        int n = sc.nextInt();
+        //int a[5];
+        int arr[] = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Enter the value of arr[" + i + "]");
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("The array is");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
