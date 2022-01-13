279179890b75f6d5f0a4f6dd70f1706b59020892
nisarg001 <nisarg0606@gmail.com>
2022-01-13 22:28:41 +0530

    Devam Java Notes and Varible code

---
+++ Devam/HelloWorld.java
@@ -0,0 +1,8 @@
+package Devam;
+
+class HelloWorld{//start of class
+    public static void main(String args[])
+    {//start of main method
+        System.out.println("Hello World");
+    }//end of main
+}//end of class
---
+++ Devam/variables.java
@@ -0,0 +1,17 @@
+package Devam;
+
+public class variables {
+    public static void main(String[] args) {
+        int a = 50;
+        int b = 50;
+        int sum = a + b;
+        int diff = a - b;
+        int mul = a * b;
+        int div = a / b;
+        System.out.println("The sum of two variables is: " + sum);
+        System.out.println("The difference between two variables is: " + diff);
+        System.out.println("The multiplication of two variables is: " + mul);
+        System.out.println("The division of two variables is: " + div);
+        // we use + symbol to differenciate variables with string of print statement
+    }
+}
