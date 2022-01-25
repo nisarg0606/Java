fc3156f6d9da9c0f498e0e1e5ca2cec13bf7f6d9
nisarg001 <nisarg0606@gmail.com>
2022-01-25 22:38:51 +0530

    Calc Completed

--- Devam/Calc.java
+++ Devam/Calc.java
@@ -11,19 +11,28 @@ public class Calc {
         int b = sc.nextInt();
         System.out.println("Enter 1 for addition");
         System.out.println("Enter 2 for substraction");
-        
+        System.out.println("Enter 3 for Multiplication");
+        System.out.println("Enter 4 for Division");
         int choice = sc.nextInt();
         if(choice == 1)
         {
             System.out.println("The addition of 2 numbers is: " + (a + b));
         }
-        if(choice == 2)
+        else if(choice == 2)
         {
             System.out.println("The substraction of 2 numbers is: " + (a - b));
+        }
+        else if(choice == 3)
+        {
+            System.out.println("The multiplication of 2 numbers is: " + (a * b));
+        }
+        else if(choice == 4)
+        {
+            System.out.println("The division of 2 numbers is: " + (a / b));
         }
         else{
             System.out.println("Please enter a valid choice....");
         }
-
+        //hw for today ----> complete all the previous code with this one
     }
 }
