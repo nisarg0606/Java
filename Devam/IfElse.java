933ae7212298f64cb8e88bb97911050f8c18298c
nisarg001 <nisarg0606@gmail.com>
2022-01-25 22:30:05 +0530

    If Else and notes

---
+++ Devam/IfElse.java
@@ -0,0 +1,17 @@
+package Devam;
+
+public class IfElse {
+    public static void main(String[] args) {
+        //if else statements are used to test the conditions. if boolean is true than if statemnt 
+        //is exexuted and if boolean = flase than //else statement is executed
+
+        int age = 18;// int is a data type and age is a variable
+        if(age >= 18)
+        {
+            System.out.println("You are eligible to vote....");
+        }
+        else{
+            System.out.println("You are not eligible to vote...");
+        }
+    }
+}
