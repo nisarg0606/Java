6a09827e20859935462bc69b7e91dd79098776f1
nisarg001 <nisarg0606@gmail.com>
2022-01-30 20:48:58 +0530

    Updating again

--- USA_Practice/Functions.java
+++ USA_Practice/Functions.java
@@ -1,8 +1,8 @@
 package USA_Practice;
 
 // Find the middle char of a string using function
-// i) if string length is odd then print 2 characters
-//  ii) if even then one character
+// i) if string length is odd then print 1 characters
+//  ii) if even then two character
 // In this program basically string's functions will be used like substring .length 
 // you need to figure out how substring function works
 // make a program to figure out how substring function works
@@ -11,8 +11,11 @@ package USA_Practice;
 class Function {
 
     public static void main(String[] args) {
-        String name = "Reyaansh";
-        name = name.substring(1, 6);
-        System.out.println(name);
+        String name = "Reyaansh";// the output should be aa
+        // name = name.substring(2, 6);// starting index to ending index - 1// the ending index is exclusive 
+        // System.out.println(name);
+        System.out.println(name.length());
+        String name1 = "Kairavy";// the output should be r
+        System.out.println(name1.length());
     }
 }
