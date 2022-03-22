54d88454986c9663d744d54074da1d256f0dc202
nisarg001 <nisarg0606@gmail.com>
2022-03-22 22:07:57 +0530

    Patterns 2.0

--- Devam/Forlooppractice.java
+++ Devam/Forlooppractice.java
@@ -18,12 +18,12 @@ public class Forlooppractice {
         }
 
         /*
-            This 
-            is 
-            a
-            multiline
-            comment
-        */
+         * This
+         * is
+         * a
+         * multiline
+         * comment
+         */
 
         // This is a single line comment
 
@@ -41,19 +41,42 @@ public class Forlooppractice {
             System.out.println();
         }
         /*
-            1 1 1 1 1
-            2 2 2 2 2
-            3 3 3 3 3
-            4 4 4 4 4
-            5 5 5 5 5
-        
-        */
+         * 1 1 1 1 1
+         * 2 2 2 2 2
+         * 3 3 3 3 3
+         * 4 4 4 4 4
+         * 5 5 5 5 5
+         * 
+         */
         /*
-        *
-        * * 
-        * * *
-        * * * *
-        * * * * *
+         *
+         * *
+         * * *
+         * * * *
+         * * * * *
+         */
+
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print(i + "  ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5; j++) {
+                System.out.print(j + "  ");
+            }
+            System.out.println();
+        }
+
+
+        /*
+        1
+        1 2
+        1 2 3
+        1 2 3 4
+        1 2 3 4 5
         */
     }
 }
