0d21bbd55df423ba5b57e0631c0e4d15177ff52f
nisarg0606 <nisarg0606@gmail.com>
2022-06-04 21:55:49 +0530

    devam

---
+++ Devam/OOPS.java
@@ -0,0 +1,10 @@
+package Devam;
+
+import java.util.Scanner;
+
+public class OOPS { // this is known as Class
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); // this thing is the object of Scanner Class
+        // to access anything of any class you need to create the object of that class
+    }
+}
